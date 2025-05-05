02fd57eb504cf37127d062fc230fd80ca60f86c3
Ya220 <118955513+Ya220@users.noreply.github.com>
2025-05-05 21:15:30 +0800

    Update todo.spec.js

--- apps/frontend/tests/todo.spec.js
+++ apps/frontend/tests/todo.spec.js
@@ -17,3 +17,19 @@ test("測試新增待辦事項", async ({ page }) => {
   expect(await todoItem.innerText()).toContain("學習 Playwright");
   expect(await todoItem.innerText()).toContain("學習 Playwright 的使用方法");
 });
+
+test("測試新增待辦事項2", async ({ page }) => {
+  // 前往 Todo App
+  await page.goto("http://localhost:3000");
+
+  // 填寫新待辦事項
+  await page.fill('input[name="title"]', "學習 test");
+  await page.fill('textarea[name="description"]', "學習 test 的使用方法");
+  await page.click('button[type="submit"]');
+
+  // 確認新增的待辦事項是否正確
+  const todoItem = await page.waitForSelector(".todo-item");
+  expect(todoItem).not.toBeNull();
+  expect(await todoItem.innerText()).toContain("學習 test");
+  expect(await todoItem.innerText()).toContain("學習 test 的使用方法");
+});
